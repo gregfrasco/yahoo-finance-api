4e27bb3494a233cec0360ab7fcec46f9df6e2e74
Greg Frasco <frascog17@gmail.com>
2020-06-13 12:03:29 -0400

    feat: change scrapping method

--- src/yahoo.ts
+++ src/yahoo.ts
@@ -1,36 +1,41 @@
-import { JSDOM } from 'jsdom';
 import { format } from 'date-fns';
-import { Earning, AnnounceTime } from './yahoo.types';
+import * as puppeteer from 'puppeteer';
+import { Earning } from './yahoo.types';
 
 const BASE_URL = 'https://finance.yahoo.com';
 
 export class YahooFinance {
   static async getEarnings(date: Date = new Date()): Promise<Earning[]> {
     const reportDate = format(date, 'yyyy-MM-dd');
-    const res = await JSDOM.fromURL(`${BASE_URL}/calendar/earnings?day=${format(date, reportDate)}`, {
-      runScripts: 'dangerously'
-    });
+    const url = `${BASE_URL}/calendar/earnings?day=${format(date, reportDate)}`;
+    return this.getEarningsTable(url, reportDate);
+  }
+
+  private static async getEarningsTable(url: string, reportDate: string): Promise<Earning[]> {
     const earnings: Earning[] = [];
-    const table = res.window.document.querySelector('tbody');
-    if (!table) {
-      return earnings;
-    }
-    for (let row of table.children) {
-      const columns = row.children;
-      const epsEstimate = Number(columns[3].textContent) || undefined;
-      const epsReported = Number(columns[4].textContent) || undefined;
-      const time = columns[2].textContent ? row.children[2].textContent : row.children[2].children[0].textContent;
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto(url);
+    const data = await page.evaluate(() => {
+      const tds = Array.from(document.querySelectorAll('table tr td'));
+      return tds.map((td: any) => td.innerText);
+    });
+    for (let i = 0; i < data.length; i += 6) {
+      const epsEstimate = data[i + 3] !== '-' ? Number(data[i + 3]) : undefined;
+      const epsReported = data[i + 4] !== '-' ? Number(data[i + 4]) : undefined;
+      const epsSurprise = data[i + 5] !== '-' ? Number(data[i + 5]) : undefined;
       earnings.push({
         epsEstimate,
         epsReported,
+        epsSurprise,
         reportDate,
-        symbol: columns[0].children[0].textContent,
-        company: columns[1].textContent,
-        announceTime: time as AnnounceTime,
-        epsSurprise: row.children[5].children[0].textContent || undefined,
+        symbol: data[i],
+        company: data[i + 1],
+        announceTime: data[i + 2],
         epsSurpriseDollar: epsReported && epsEstimate ? Number((epsReported - epsEstimate).toFixed(2)) : undefined
       });
     }
+    await browser.close();
     return earnings;
   }
 }
--- test/yahoo.spec.ts
+++ test/yahoo.spec.ts
@@ -1,13 +1,43 @@
-import { YahooFinance } from '../src';
-import {addDays} from "date-fns";
+import { AnnounceTime, YahooFinance } from '../src';
 
 describe('Yahoo Finance', () => {
-
-    describe('getEarnings', () => {
-
-        it('successfully gets earnings', async () => {
-           const earnings = await YahooFinance.getEarnings(addDays(new Date(), 2));
-           expect(earnings).toBeDefined();
-        });
+  describe('getEarnings', () => {
+    it('successfully gets earnings', async () => {
+      const earnings = await YahooFinance.getEarnings(new Date('2020-05-30T23:00:00.000Z'));
+      expect(earnings).toBeDefined();
+      expect(earnings).toHaveLength(3);
+      expect(earnings).toEqual([
+        {
+          epsEstimate: -0.1,
+          epsReported: -0.65,
+          epsSurprise: -550,
+          reportDate: '2020-05-30',
+          symbol: 'LTMAQ',
+          company: 'LATAM Airlines Group S.A.',
+          announceTime: AnnounceTime.TIME_NOT_SUPPLIED,
+          epsSurpriseDollar: -0.55
+        },
+        {
+          epsEstimate: -0.1,
+          epsReported: -0.65,
+          epsSurprise: -550,
+          reportDate: '2020-05-30',
+          symbol: 'LTM',
+          company: 'LATAM Airlines Group S.A.',
+          announceTime: AnnounceTime.TIME_NOT_SUPPLIED,
+          epsSurpriseDollar: -0.55
+        },
+        {
+          epsEstimate: 0.17,
+          epsReported: 0.28,
+          epsSurprise: 64.71,
+          reportDate: '2020-05-30',
+          symbol: 'CALM',
+          company: 'Cal-Maine Foods Inc',
+          announceTime: AnnounceTime.TIME_NOT_SUPPLIED,
+          epsSurpriseDollar: 0.11
+        }
+      ]);
     });
+  });
 });
